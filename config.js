02555178f2b163371eb861d905689a6981efe305
mishumd622-cloud <mishumd622@gmail.com>
2026-07-22 11:31:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/mishumd622-cloud/mishu-md-v1/blob/main/images/IMG-20260721-WA0054.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*(𝓗ᴇʟʟᴏᴡ) ᴍɪꜱʜᴜ-ᴍᴅ ɪꜱ ᴀʟɪᴠᴇ ɴᴏᴡ 🙇🏻‍♂️💨"*",
+BOT_OWNER: '94724567955',  // Replace with the owner's phone number
 
 
 
